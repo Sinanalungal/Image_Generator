c73d3d68997f60a26afb4e42fa61d04f17c74b6e
sinan <sinanalungal012@gmail.com>
2024-02-09 11:00:15 +0530

    login and signup design just done not fully completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './Components/Login/Login'
+import SignUp from './Components/SignUp/SignUp'
+function App() {
+  return (
+    <>
+    {/* <Login/> */}
+    <SignUp/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/SignUp/SignUp.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from '../../assets/Animation - 1707454838945.json'; // Make sure to provide the correct path to your animation file
+import "../Login/Login.css";
+
+function SignUp() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  return (
+    <>
+      <div className='w-full h-svh flex'>
+        <div className='w-[60%] h-full rounded-r-xl grid items-center justify-center'>
+          {/* <h1 className='text-3xl font-bold '>Image Generator</h1> */}
+          {/* <Lottie options={defaultOptions} height={400} width={400} /> */}
+        </div>
+        <div className='w-[40%] h-full bg-slate-200 rounded-l-lg flex justify-center items-center'>
+          <section className="section_form">
+            <h1 className='text-3xl font-bold'>SIGN UP</h1>
+            <form id="consultation-form" className="feed-form" action="#">
+              <input required="" placeholder="Username" type="text" />
+              <input name="phone" required="" placeholder="Phone number" />
+              <input name="email" required="" placeholder="E-mail" type="email" />
+              <input name="password" required="" placeholder="Password" type="password" />
+              <input name="confim-password" required="" placeholder="Confirm Password" type="password" />
+              <button className="button_submit">ORDER</button>
+            </form>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SignUp;
